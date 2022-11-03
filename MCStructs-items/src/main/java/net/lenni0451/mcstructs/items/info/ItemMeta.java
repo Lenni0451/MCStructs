b4a03b6b7b8a47cc4c34eaff075d3dffb358ba78
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2022-11-03 22:02:37 +0100

    Added max damage

--- MCStructs-items/src/main/java/net/lenni0451/mcstructs/items/info/ItemMeta.java
+++ MCStructs-items/src/main/java/net/lenni0451/mcstructs/items/info/ItemMeta.java
@@ -10,7 +10,17 @@ public class ItemMeta {
         private final String exceptionMessage = "Unable to modify empty item meta";
 
         @Override
+        public ItemMeta types(ItemType... types) {
+            throw new UnsupportedOperationException(this.exceptionMessage);
+        }
+
+        @Override
         public ItemMeta maxCount(int maxStackSize) {
+            throw new UnsupportedOperationException(this.exceptionMessage);
+        }
+
+        @Override
+        public ItemMeta maxDamage(int maxDamage) {
             throw new UnsupportedOperationException(this.exceptionMessage);
         }
 
@@ -18,11 +28,17 @@ public class ItemMeta {
         public ItemMeta damageable(boolean damageable) {
             throw new UnsupportedOperationException(this.exceptionMessage);
         }
+
+        @Override
+        public ItemMeta hasSubtypes(boolean hasSubtypes) {
+            throw new UnsupportedOperationException(this.exceptionMessage);
+        }
     };
 
 
     private List<ItemType> types = new ArrayList<>();
     private int maxCount = 64;
+    private int maxDamage = 0;
     private boolean damageable = false;
     private boolean hasSubtypes = false;
 
@@ -45,6 +61,15 @@ public class ItemMeta {
 
     public ItemMeta maxCount(final int maxStackSize) {
         this.maxCount = maxStackSize;
+        return this;
+    }
+
+    public int maxDamage() {
+        return this.maxDamage;
+    }
+
+    public ItemMeta maxDamage(final int maxDamage) {
+        this.maxDamage = maxDamage;
         return this;
     }
 
