6d1124cc822a14147917d99c6553fa572d52eeb5
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2022-11-09 18:12:15 +0100

    Added HopperContainer_v1_7 and HopperInventory_v1_7

--- MCStructs-inventory/src/main/java/net/lenni0451/mcstructs/inventory/impl/v1_7/container/HopperContainer_v1_7.java
+++ MCStructs-inventory/src/main/java/net/lenni0451/mcstructs/inventory/impl/v1_7/container/HopperContainer_v1_7.java
@@ -3,21 +3,21 @@ package net.lenni0451.mcstructs.inventory.impl.v1_7.container;
 import net.lenni0451.mcstructs.inventory.InventoryHolder;
 import net.lenni0451.mcstructs.inventory.Slot;
 import net.lenni0451.mcstructs.inventory.impl.v1_7.AContainer_v1_7;
-import net.lenni0451.mcstructs.inventory.impl.v1_7.inventory.DispenserInventory_v1_7;
+import net.lenni0451.mcstructs.inventory.impl.v1_7.inventory.HopperInventory_v1_7;
 import net.lenni0451.mcstructs.inventory.impl.v1_7.inventory.PlayerInventory_v1_7;
 import net.lenni0451.mcstructs.items.stacks.LegacyItemStack;
 
-public class DispenserContainer_v1_7<I> extends AContainer_v1_7<I> {
+public class HopperContainer_v1_7<I> extends AContainer_v1_7<I> {
 
     private final PlayerInventory_v1_7<I> playerInventory;
-    private final DispenserInventory_v1_7<I> dispenserInventory;
+    private final HopperInventory_v1_7<I> hopperInventory;
 
-    public DispenserContainer_v1_7(final int windowId, final PlayerInventory_v1_7<I> playerInventory) {
+    public HopperContainer_v1_7(final int windowId, final PlayerInventory_v1_7<I> playerInventory) {
         super(windowId);
         this.playerInventory = playerInventory;
-        this.dispenserInventory = new DispenserInventory_v1_7<>();
+        this.hopperInventory = new HopperInventory_v1_7<>();
 
-        for (int i = 0; i < 9; i++) this.addSlot(this.dispenserInventory, i, Slot.acceptAll());
+        for (int i = 0; i < 5; i++) this.addSlot(this.hopperInventory, i, Slot.acceptAll());
         for (int i = 0; i < 27; i++) this.addSlot(this.playerInventory, 9 + i, Slot.acceptAll());
         for (int i = 0; i < 9; i++) this.addSlot(this.playerInventory, i, Slot.acceptAll());
     }
@@ -26,8 +26,8 @@ public class DispenserContainer_v1_7<I> extends AContainer_v1_7<I> {
         return this.playerInventory;
     }
 
-    public DispenserInventory_v1_7<I> getDispenserInventory() {
-        return this.dispenserInventory;
+    public HopperInventory_v1_7<I> getHopperInventory() {
+        return this.hopperInventory;
     }
 
     @Override
@@ -37,15 +37,13 @@ public class DispenserContainer_v1_7<I> extends AContainer_v1_7<I> {
 
         LegacyItemStack<I> slotStack = slot.getStack();
         LegacyItemStack<I> out = slotStack.copy();
-        if (slotId <= 8) {
-            if (!this.mergeStack(slotStack, 9, 45, true)) return null;
-        } else if (!this.mergeStack(slotStack, 0, 9, false)) {
+        if (slotId < this.hopperInventory.getSize()) {
+            if (!this.mergeStack(slotStack, 0, this.hopperInventory.getSize(), true)) return null;
+        } else if (!this.mergeStack(slotStack, 0, this.hopperInventory.getSize(), false)) {
             return null;
         }
         if (slotStack.getCount() == 0) slot.setStack(null);
         else slot.onUpdate();
-        if (slotStack.getCount() == out.getCount()) return null;
-        slot.onTake(inventoryHolder, slotStack);
         return out;
     }
 
---
+++ MCStructs-inventory/src/main/java/net/lenni0451/mcstructs/inventory/impl/v1_7/inventory/HopperInventory_v1_7.java
@@ -0,0 +1,44 @@
+package net.lenni0451.mcstructs.inventory.impl.v1_7.inventory;
+
+import net.lenni0451.mcstructs.inventory.impl.v1_7.IInventory_v1_7;
+import net.lenni0451.mcstructs.items.stacks.LegacyItemStack;
+
+public class HopperInventory_v1_7<I> implements IInventory_v1_7<I> {
+
+    private final LegacyItemStack<I>[] stacks = new LegacyItemStack[5];
+
+    public LegacyItemStack<I>[] getStacks() {
+        return this.stacks;
+    }
+
+    @Override
+    public LegacyItemStack<I> split(int slotId, int count) {
+        if (this.stacks[slotId] == null) return null;
+
+        LegacyItemStack<I> stack = this.stacks[slotId];
+        if (stack.getCount() <= count) {
+            this.stacks[slotId] = null;
+        } else {
+            stack = stack.split(count);
+            if (stack.getCount() == 0) this.stacks[slotId] = null;
+        }
+        return stack;
+    }
+
+    @Override
+    public int getSize() {
+        return this.stacks.length;
+    }
+
+    @Override
+    public LegacyItemStack<I> getStack(int slotId) {
+        return this.stacks[slotId];
+    }
+
+    @Override
+    public void setStack(int slotId, LegacyItemStack<I> stack) {
+        this.stacks[slotId] = stack;
+        if (stack != null && stack.getCount() > 64) stack.setCount(64);
+    }
+
+}
