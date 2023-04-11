e6ecc51cb774d8940b33127dd745a22f50cad3ee
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2023-04-11 21:20:33 +0200

    Improved javadoc

--- MCStructs-nbt/src/main/java/net/lenni0451/mcstructs/nbt/io/internal/NbtReadWrapper.java
+++ MCStructs-nbt/src/main/java/net/lenni0451/mcstructs/nbt/io/internal/NbtReadWrapper.java
@@ -5,6 +5,8 @@ import net.lenni0451.mcstructs.nbt.io.NbtHeader;
 import net.lenni0451.mcstructs.nbt.io.NbtReadTracker;
 import net.lenni0451.mcstructs.nbt.io.types.INbtReader;
 
+import javax.annotation.Nonnull;
+import javax.annotation.WillNotClose;
 import java.io.*;
 import java.nio.file.Files;
 import java.util.zip.GZIPInputStream;
@@ -24,7 +26,7 @@ public interface NbtReadWrapper {
      * @return The read tag
      * @throws IOException If an I/O error occurs
      */
-    default INbtTag readFile(final File f, final NbtReadTracker readTracker) throws IOException {
+    default INbtTag readFile(final File f, @Nonnull final NbtReadTracker readTracker) throws IOException {
         return this.readFile(f, false, readTracker);
     }
 
@@ -36,7 +38,7 @@ public interface NbtReadWrapper {
      * @return The read tag
      * @throws IOException If an I/O error occurs
      */
-    default INbtTag readCompressedFile(final File f, final NbtReadTracker readTracker) throws IOException {
+    default INbtTag readCompressedFile(final File f, @Nonnull final NbtReadTracker readTracker) throws IOException {
         return this.readFile(f, true, readTracker);
     }
 
@@ -49,15 +51,14 @@ public interface NbtReadWrapper {
      * @return The read tag
      * @throws IOException If an I/O error occurs
      */
-    default INbtTag readFile(final File f, final boolean compressed, final NbtReadTracker readTracker) throws IOException {
+    default INbtTag readFile(final File f, final boolean compressed, @Nonnull final NbtReadTracker readTracker) throws IOException {
         try (InputStream fis = Files.newInputStream(f.toPath())) {
             return this.read(fis, compressed, readTracker);
         }
     }
 
     /**
-     * Read a Nbt tag from an input stream.<br>
-     * The stream will <b>not</b> be closed.
+     * Read a Nbt tag from an input stream.
      *
      * @param is          The input stream to read from
      * @param compressed  Whether the input stream is compressed or not
@@ -65,21 +66,22 @@ public interface NbtReadWrapper {
      * @return The read tag
      * @throws IOException If an I/O error occurs
      */
-    default INbtTag read(final InputStream is, final boolean compressed, final NbtReadTracker readTracker) throws IOException {
+    @WillNotClose
+    default INbtTag read(final InputStream is, final boolean compressed, @Nonnull final NbtReadTracker readTracker) throws IOException {
         if (compressed) return this.read(new DataInputStream(new GZIPInputStream(is)), readTracker);
         else return this.read(new DataInputStream(is), readTracker);
     }
 
     /**
-     * Read a Nbt tag from a {@link DataInput}.<br>
-     * The data input will <b>not</b> be closed.
+     * Read a Nbt tag from a {@link DataInput}.
      *
      * @param in          The data input to read from
      * @param readTracker The read tracker to use
      * @return The read tag
      * @throws IOException If an I/O error occurs
      */
-    default INbtTag read(final DataInput in, final NbtReadTracker readTracker) throws IOException {
+    @WillNotClose
+    default INbtTag read(final DataInput in, @Nonnull final NbtReadTracker readTracker) throws IOException {
         NbtHeader header = this.getReader().readHeader(in, readTracker);
         if (header.isEnd()) return null;
         readTracker.pushDepth();
--- MCStructs-nbt/src/main/java/net/lenni0451/mcstructs/nbt/io/internal/NbtWriteWrapper.java
+++ MCStructs-nbt/src/main/java/net/lenni0451/mcstructs/nbt/io/internal/NbtWriteWrapper.java
@@ -1,11 +1,12 @@
 package net.lenni0451.mcstructs.nbt.io.internal;
 
 import net.lenni0451.mcstructs.nbt.INbtTag;
-import net.lenni0451.mcstructs.nbt.NbtType;
 import net.lenni0451.mcstructs.nbt.io.NbtHeader;
 import net.lenni0451.mcstructs.nbt.io.types.INbtWriter;
 
 import javax.annotation.Nonnull;
+import javax.annotation.WillClose;
+import javax.annotation.WillNotClose;
 import java.io.*;
 import java.nio.file.Files;
 import java.util.zip.GZIPOutputStream;
@@ -21,7 +22,7 @@ public interface NbtWriteWrapper {
      * Write and uncompressed Nbt tag to a file.
      *
      * @param f    The file to write to
-     * @param name The name of the tag (should be "" if not needed)
+     * @param name The name of the tag (should be "" if not required)
      * @param tag  The tag to write
      * @throws IOException If an I/O error occurs
      */
@@ -33,11 +34,11 @@ public interface NbtWriteWrapper {
      * Write a compressed Nbt tag to a file.
      *
      * @param f    The file to write to
-     * @param name The name of the tag (should be "" if not needed)
+     * @param name The name of the tag (should be "" if not required)
      * @param tag  The tag to write
      * @throws IOException If an I/O error occurs
      */
-    default void writeCompressedFile(final File f, final String name, final INbtTag tag) throws IOException {
+    default void writeCompressedFile(final File f, @Nonnull final String name, final INbtTag tag) throws IOException {
         this.writeFile(f, name, tag, true);
     }
 
@@ -45,27 +46,28 @@ public interface NbtWriteWrapper {
      * Write a Nbt tag to a file.
      *
      * @param f          The file to write to
-     * @param name       The name of the tag (should be "" if not needed)
+     * @param name       The name of the tag (should be "" if not required)
      * @param tag        The tag to write
      * @param compressed Whether the file should be compressed or not
      * @throws IOException If an I/O error occurs
      */
-    default void writeFile(final File f, final String name, final INbtTag tag, final boolean compressed) throws IOException {
+    default void writeFile(final File f, @Nonnull final String name, final INbtTag tag, final boolean compressed) throws IOException {
         try (OutputStream fos = Files.newOutputStream(f.toPath())) {
             this.write(fos, name, tag, compressed);
         }
     }
 
     /**
-     * Write a Nbt tag to an output stream.<br>
+     * Write a Nbt tag to an output stream.
      *
      * @param os         The output stream to write to
-     * @param name       The name of the tag (should be "" if not needed)
+     * @param name       The name of the tag (should be "" if not required)
      * @param tag        The tag to write
      * @param compressed Whether the output stream should be compressed or not
      * @throws IOException If an I/O error occurs
      */
-    default void write(final OutputStream os, final String name, final INbtTag tag, final boolean compressed) throws IOException {
+    @WillClose
+    default void write(final OutputStream os, @Nonnull final String name, final INbtTag tag, final boolean compressed) throws IOException {
         if (compressed) {
             try (GZIPOutputStream gos = new GZIPOutputStream(os)) {
                 this.write(new DataOutputStream(gos), name, tag);
@@ -76,16 +78,16 @@ public interface NbtWriteWrapper {
     }
 
     /**
-     * Write a Nbt tag to a {@link DataOutput}.<br>
-     * The data output will <b>not</b> be closed.
+     * Write a Nbt tag to a {@link DataOutput}.
      *
      * @param out  The data output to write to
-     * @param name The name of the tag (should be "" if not needed)
+     * @param name The name of the tag (should be "" if not required)
      * @param tag  The tag to write
      * @throws IOException If an I/O error occurs
      */
-    default void write(final DataOutput out, final String name, final INbtTag tag) throws IOException {
-        this.getWriter().writeHeader(out, new NbtHeader(NbtType.byClass(tag.getClass()), name));
+    @WillNotClose
+    default void write(final DataOutput out, @Nonnull final String name, final INbtTag tag) throws IOException {
+        this.getWriter().writeHeader(out, new NbtHeader(tag.getNbtType(), name));
         this.getWriter().write(out, tag);
     }
 
