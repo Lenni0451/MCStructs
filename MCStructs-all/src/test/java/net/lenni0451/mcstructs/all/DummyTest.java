f6709101f41a8ab4c86ac73a86d2782624cb2eb3
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2024-01-01 21:04:54 +0100

    Fixed gradle warnings

---
+++ MCStructs-all/src/test/java/net/lenni0451/mcstructs/all/DummyTest.java
@@ -0,0 +1,12 @@
+package net.lenni0451.mcstructs.all;
+
+import org.junit.jupiter.api.Test;
+
+public class DummyTest {
+
+    @Test
+    void dummyTest() {
+        //Dummy test to stop gradle from complaining
+    }
+
+}
