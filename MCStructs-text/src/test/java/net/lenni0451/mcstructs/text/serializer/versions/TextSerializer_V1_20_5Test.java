6c9293146ba11884d605c5dfea2611d7a4718aef
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2024-04-20 18:51:43 +0200

    Added missing test class

---
+++ MCStructs-text/src/test/java/net/lenni0451/mcstructs/text/serializer/versions/TextSerializer_V1_20_5Test.java
@@ -0,0 +1,47 @@
+package net.lenni0451.mcstructs.text.serializer.versions;
+
+import net.lenni0451.mcstructs.text.ATextComponent;
+import net.lenni0451.mcstructs.text.Style;
+import net.lenni0451.mcstructs.text.components.StringComponent;
+import net.lenni0451.mcstructs.text.components.TranslationComponent;
+import org.junit.jupiter.api.Test;
+
+import static net.lenni0451.mcstructs.text.serializer.TextComponentSerializer.V1_20_5;
+
+public class TextSerializer_V1_20_5Test extends TextSerializerTest {
+
+    @Override
+    protected ATextComponent deserialize(String json) {
+        return V1_20_5.deserializeParser(json);
+    }
+
+    @Override
+    protected String serialize(ATextComponent component) {
+        return V1_20_5.serialize(component);
+    }
+
+    @Test
+    @Override
+    protected void runTests() {
+        this.executeDeserializeTests(
+                new StringComponent("test"),
+                new StringComponent("test"),
+                DESERIALIZE_FAIL,
+                DESERIALIZE_FAIL,
+                DESERIALIZE_FAIL,
+                DESERIALIZE_FAIL,
+                new StringComponent("\0"),
+                DESERIALIZE_FAIL,
+                new TranslationComponent("test"),
+                new StringComponent("test1"),
+                new TranslationComponent("test2"),
+                new TranslationComponent("%s %s", "abc", 123),
+                new StringComponent("test").setStyle(new Style().setColor(0xFFF001)),
+                DESERIALIZE_FAIL
+        );
+        this.executeSerializeTests(
+                "\"test\""
+        );
+    }
+
+}
