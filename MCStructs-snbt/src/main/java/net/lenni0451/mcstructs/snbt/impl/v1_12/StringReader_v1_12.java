202ccd707c15837a6e8fa84d65a1c8fd684fe0da
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2024-08-30 18:47:09 +0200

    Implemented more methods

--- MCStructs-snbt/src/main/java/net/lenni0451/mcstructs/snbt/impl/v1_12/StringReader_v1_12.java
+++ MCStructs-snbt/src/main/java/net/lenni0451/mcstructs/snbt/impl/v1_12/StringReader_v1_12.java
@@ -47,6 +47,30 @@ public class StringReader_v1_12 {
         while (this.canRead() && Character.isWhitespace(this.peek())) this.index++;
     }
 
+    public int readInt() throws SNbtDeserializeException {
+        int start = this.index;
+        while (this.canRead() && this.isNumerical(this.peek())) this.index++;
+        String number = this.s.substring(start, this.index);
+        if (number.isEmpty()) throw new SNbtDeserializeException("Expected double but got nothing", this.s, this.index);
+        try {
+            return Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            throw new SNbtDeserializeException("Expected double but got '" + number + "'", this.s, start);
+        }
+    }
+
+    public double readDouble() throws SNbtDeserializeException {
+        int start = this.index;
+        while (this.canRead() && this.isNumerical(this.peek())) this.index++;
+        String number = this.s.substring(start, this.index);
+        if (number.isEmpty()) throw new SNbtDeserializeException("Expected double but got nothing", this.s, this.index);
+        try {
+            return Double.parseDouble(number);
+        } catch (NumberFormatException e) {
+            throw new SNbtDeserializeException("Expected double but got '" + number + "'", this.s, start);
+        }
+    }
+
     public String readString() throws SNbtDeserializeException {
         this.skipWhitespaces();
         if (!this.canRead()) return null;
@@ -93,6 +117,10 @@ public class StringReader_v1_12 {
 
     protected boolean isQuote(final char c) {
         return c == '"';
+    }
+
+    private boolean isNumerical(final char c) {
+        return c >= '0' && c <= '9' || c == '.' || c == '-';
     }
 
     private boolean isAlphanumeric(final char c) {
