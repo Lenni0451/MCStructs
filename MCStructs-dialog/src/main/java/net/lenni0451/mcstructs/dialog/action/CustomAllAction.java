c9ef7722cc5c57339709670ed44c607397e0804b
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2025-06-09 18:05:29 +0200

    Added support for mapping actions to click events

--- MCStructs-dialog/src/main/java/net/lenni0451/mcstructs/dialog/action/CustomAllAction.java
+++ MCStructs-dialog/src/main/java/net/lenni0451/mcstructs/dialog/action/CustomAllAction.java
@@ -4,8 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import net.lenni0451.mcstructs.core.Identifier;
 import net.lenni0451.mcstructs.nbt.tags.CompoundTag;
+import net.lenni0451.mcstructs.text.events.click.ClickEvent;
 
 import javax.annotation.Nullable;
+import java.util.Map;
 
 @Data
 @AllArgsConstructor
@@ -14,5 +16,15 @@ public class CustomAllAction implements DialogAction {
     private Identifier id;
     @Nullable
     private CompoundTag additions;
+
+    @Nullable
+    @Override
+    public ClickEvent toAction(Map<String, ValueGetter> valueGetters) {
+        CompoundTag tag = this.additions == null ? new CompoundTag() : this.additions.copy();
+        for (Map.Entry<String, ValueGetter> entry : valueGetters.entrySet()) {
+            tag.add(entry.getKey(), entry.getValue().asNbtTag());
+        }
+        return ClickEvent.custom(this.id, tag);
+    }
 
 }
--- MCStructs-dialog/src/main/java/net/lenni0451/mcstructs/dialog/action/StaticAction.java
+++ MCStructs-dialog/src/main/java/net/lenni0451/mcstructs/dialog/action/StaticAction.java
@@ -4,10 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import net.lenni0451.mcstructs.text.events.click.ClickEvent;
 
+import javax.annotation.Nullable;
+import java.util.Map;
+
 @Data
 @AllArgsConstructor
 public class StaticAction implements DialogAction {
 
     private ClickEvent value;
+
+    @Nullable
+    @Override
+    public ClickEvent toAction(Map<String, ValueGetter> valueGetters) {
+        return this.value;
+    }
 
 }
